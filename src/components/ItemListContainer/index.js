729af10372a46f85f6144350fa8b672c61b9c35f
Octavio Peralta <octi.pro20@gmail.com>
2021-03-15 13:52:59 -0300

    Agrego ItemCount

--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ItemCount from '../ItemCount'
+
 import "./ItemListContainer.scss"
+
+
 const ItemListContainer = (props) => {
     return (
         <div className="container">
-            <h3>{props.greeting}</h3>
+            <ItemCount initial={1} stock={3}></ItemCount>
+            <ItemCount initial={2} stock={10}></ItemCount>
         </div>
     )
 
